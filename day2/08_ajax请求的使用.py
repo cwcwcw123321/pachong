eb62b0e73e609e84f8adf677293e412d2d5d01c9
cwcwcw123321 <qihaoh@163.com>
2020-04-10 19:21:24 +0800

    get 请求

--- day2/08_ajax请求的使用.py
+++ day2/08_ajax请求的使用.py
@@ -1,4 +1,14 @@
 from urllib.request import Request,urlopen
-from
+from fake_useragent import UserAgent
 
-url='https://movie.douban.com/j/chart/top_list?type=11&interval_id=100%3A90&action=&start=0&limit=1'
+base_url='https://movie.douban.com/j/chart/top_list?type=11&interval_id=100%3A90&action=&start={}&limit=20'
+
+for i in range(10):
+
+    headers={
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0'
+    }
+    url=base_url.format(i*20)
+    request=Request(url,headers=headers)
+    response=urlopen(request)
+    print(response.read().decode())
