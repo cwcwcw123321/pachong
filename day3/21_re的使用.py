bfc08699223d43dbeca90d5bbffa920074fd6dac
Administrator <qihaoh@163.com>
2020-04-13 17:02:00 +0800

    re 正则

---
+++ day3/21_re的使用.py
@@ -0,0 +1,37 @@
+import re
+
+str1="I Study Python3.8 Everyday"
+print('---------------match()-------------------')
+m1=re.match(r'I',str1)
+m2=re.match(r'\w',str1)
+m3=re.match(r'.',str1)
+m4=re.match(r'\D',str1)
+m5=re.match(r'i',str1,re.I)
+m6=re.match(r'\S',str1)
+#m7=re.match(r'Study',str1) #匹配不到，因为match是从左开始匹配
+print(m1.group())
+
+print('---------------search()-------------------')
+s1=re.search(r'Study',str1)
+s2=re.search(r'S\w+',str1)
+s3=re.search(r'Python3.8',str1)
+s4=re.search(r'P\w+.\d',str1)
+
+print(s3.group())
+
+print('---------------findall()-------------------')
+f1=re.findall(r'y',str1)
+
+print(f1)
+
+print('---------------test()-------------------')
+str2="<div><a href='www.bjsxt.com'>bjsxt尚学堂</a></div>"
+
+t1=re.findall(r'[\u4e00-\u9fa5]\w+',str2)
+t2=re.findall(r"<a herf='www.bjsxt.com'>(.+)</a>",str2)
+t3 = re.findall(r"<a href='(.+)'>", str2)
+print(t3)
+print('---------------sub()-------------------')
+su1=re.sub(r'<div>(.+)</div>',r'<span>\1</span>',str2)
+
+print(su1)
