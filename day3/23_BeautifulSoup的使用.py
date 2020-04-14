0bd4e895f6aa76267b5672dcc919aebff48f80e9
Administrator <qihaoh@163.com>
2020-04-14 10:26:21 +0800

    bs4

--- day3/23_BeautifulSoup的使用.py
+++ day3/23_BeautifulSoup的使用.py
@@ -29,4 +29,28 @@ if type(soup.strong.string)==Comment:
 else:
     print(soup.strong.text)
 
+print("-----------------------fina_all-------------------------")
+print(soup.find_all('title'))
+print(soup.find_all(id="title"))
+print(soup.find_all(class_="info"))
+print(soup.find_all("div",attrs={'float':'left'}))
+
 # css 标签名不加任何修饰，类名前加点，id名前加 #
+str='''
+<title id="title">尚学堂</title>
+<div class='info' float='left'>Welcome to SXT</div>
+<div class='info' float='right'>
+    <span>Good Good Study</span>
+    <a href='www.bjsxt.cn'></a>
+    <strong><!--没用--></strong>
+</div>
+'''
+print("-----------------------css 选择器-------------------------")
+# css 标签名不加任何修饰，类名前加点，id名前加 #
+print(soup.select('title'))
+print(soup.select('#title'))
+print(soup.select('.info'))
+print(soup.select('div span'))
+print(soup.select('div>span'))
+print(soup.select('div')[1].select('a'))
+print(soup.select('title')[0].text)
