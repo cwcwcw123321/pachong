cb90d677bd2365461add8927eed88aa21508b5f3
Administrator <qihaoh@163.com>
2020-04-29 15:49:23 +0800

    动态ua

--- tupian/tupian/settings.py
+++ tupian/tupian/settings.py
@@ -16,7 +16,14 @@ NEWSPIDER_MODULE = 'tupian.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'
+# USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'
+USER_AGENTS=[
+   # 'UA1',
+   # 'UA2',
+   # 'UA3'
+]
+
+
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
@@ -52,9 +59,9 @@ ROBOTSTXT_OBEY = False
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'tupian.middlewares.TupianDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   'tupian.middlewares.UserAgentDownloadMiddleware': 343,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
--- tupian/tupian/start.py
+++ tupian/tupian/start.py
@@ -1,3 +1,4 @@
 from scrapy.cmdline import execute
 
-execute('scrapy crawl zol'.split())
+# execute('scrapy crawl zol'.split())
+execute('scrapy crawl http_ua'.split())
